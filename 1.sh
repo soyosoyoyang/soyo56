5635454a8c5bca30f81da45398b0f412c3d06d3b
soyosoyoyang <15561566591@163.com>
2019-07-18 11:41:47 +0800

    9

---
+++ 1.sh
@@ -0,0 +1,4 @@
+find /home/soyo/文档/testvideos/ -name "*.mp4" -type f -mmin -1  > soyo_vid2.txt
+find /home/soyo/文档/testvideos2/ -name "*.mp4" -type f -mmin -1  > soyo_vid3.txt
+find /home/soyo/文档/testvideos3/ -name "*.mp4" -type f -mmin -1  > soyo_vid4.txt
+find /home/soyo/文档/testvideos4/ -name "*.mp4" -type f -mmin -1  > soyo_vid5.txt
